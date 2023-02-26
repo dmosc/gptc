2e0b2ebb7d132b12372e26e9e3098f3ebb8baa25
dmosc <osdavrm@gmail.com>
2023-02-25 22:34:44 -0600

    Remove ASCII escape characters since they aren't compatible on most OS shells.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 
-RED='\033[0;31m';
-BLUE="\033[0;34m";
-YELLOW="\033[1;33m";
-NO_COLOR="\033[0m";
-
 info() {
-	echo "${YELLOW}[info]: $@${NO_COLOR}";
+	echo "[info]: $@";
 }
 
 message() {
-	echo "${BLUE}[message]: $@${NO_COLOR}";
+	echo "[message]: $@";
 }
 
 error() {
-	echo "${RED}[error]: $@${NO_COLOR}";
+	echo "[error]: $@";
 	exit 1;
 }
 
